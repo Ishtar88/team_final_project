fd6756c38a8409cee99b5bbd9f09904f0c66cdd4
Ishtar88 <32984886+Ishtar88@users.noreply.github.com>
2018-03-14 14:38:06 +0900

    ㅠㅠ

---
+++ final_project/src/main/java/com/jsr/project/services/IManagerService.java
@@ -0,0 +1,11 @@
+package com.jsr.project.services;
+
+import java.util.List;
+
+import com.jsr.project.dtos.MembersDto;
+
+public interface IManagerService {
+	
+	public List<MembersDto> getAllMember();
+
+}
